4e948cde4f2de8a400b0934406478316e7e713bd
Christian Kvalheim <christkv@gmail.com>
2015-06-08 16:17:09 +0200

    Added replaceOne method and tests to crud api

--- test/crud_tests.js
+++ test/crud_tests.js
@@ -24,7 +24,11 @@ var createTestExecutor = function(instance, method, data, setup, teardown) {
   }
 }
 
-var data = [{_id: 1, x:11}, {_id: 2, x:22}, {_id: 3, x:33}];
+var data = [{ _id: 1, x:11 }, { _id: 2, x:22 }, { _id: 3, x:33 }];
+var findOneData = [{ _id:1, x:11 }];
+var replaceOneData = [{ _id: 1, x: 11 }, { _id: 2, x: 22 }, { _id:3, x:33 }];
+
+// Setup method
 var setup = function(col, method, data) {
   col.remove({});
   col.insertMany(data);  
@@ -36,6 +40,9 @@ var deleteOneExecutor = createTestExecutor(col, 'deleteOne', data, setup);
 var findOneAndDeleteExecutor = createTestExecutor(col, 'findOneAndDelete', data, setup);
 var findOneAndReplaceExecutor = createTestExecutor(col, 'findOneAndReplace', data, setup);
 var findOneAndUpdateExecutor = createTestExecutor(col, 'findOneAndUpdate', data, setup);
+var insertManyExecutor = createTestExecutor(col, 'insertMany', findOneData, setup);
+var insertOneExecutor = createTestExecutor(col, 'insertOne', findOneData, setup);
+var replaceOneExecutor = createTestExecutor(col, 'replaceOne', replaceOneData, setup);
 
 //
 // DeleteMany
@@ -110,9 +117,33 @@ findOneAndUpdateExecutor([{ _id: 4 }, { $inc: { x: 1 } }, { projection: { x: 1, 
 // FindOneAndUpdate when no documents match with upsert returning the document after modification
 findOneAndUpdateExecutor([{ _id: 4 }, { $inc: { x: 1 } }, { projection: { x: 1, _id: 0 }, sort: { x: 1 }, returnDocument:true, upsert:true }], {x:1}, [{_id:1, x: 11}, {_id:2, x: 22}, {_id:3, x: 33}, {_id:4, x: 1}]);
 
+//
+// InsertMany
+//
 
+// InsertMany with non-existing documents
+insertManyExecutor([[{_id: 2, x: 22}, {_id:3, x:33}]], {acknowledged: true, insertedIds: [2, 3]}, [{_id:1, x: 11}, {_id:2, x: 22}, {_id:3, x: 33}]);
 
+//
+// InsertOne
+//
 
+// InsertMany with non-existing documents
+insertOneExecutor([{_id: 2, x: 22}], {acknowledged: true, insertedId: 2}, [{_id:1, x: 11}, {_id:2, x: 22}]);
 
+//
+// ReplaceOne
+//
+
+// ReplaceOne when many documents match
+replaceOneExecutor([{ _id: { $gt: 1 } }, { x: 111 }], {acknowledged:true, matchedCount:1, modifiedCount:1}, [{_id:1, x: 11}, {_id:2, x: 111}, {_id:3, x: 33}]);
+// ReplaceOne when one document matches
+replaceOneExecutor([{ _id: 1 }, { _id: 1, x: 111 }], {acknowledged:true, matchedCount:1, modifiedCount:1}, [{_id:1, x: 111}, {_id:2, x: 22}, {_id:3, x: 33}]);
+// ReplaceOne when no documents match
+replaceOneExecutor([{ _id: 4 }, { _id: 4, x: 1 }], {acknowledged:true, matchedCount:0, modifiedCount:0}, [{_id:1, x: 11}, {_id:2, x: 22}, {_id:3, x: 33}]);
+// ReplaceOne with upsert when no documents match without an id specified
+replaceOneExecutor([{ _id: 4 }, { x: 1 }, {upsert:true}], {acknowledged:true, matchedCount:0, modifiedCount:0, upsertedId: 4}, [{_id:1, x: 11}, {_id:2, x: 22}, {_id:3, x: 33}, {_id:4, x: 1}]);
+// ReplaceOne with upsert when no documents match with an id specified
+replaceOneExecutor([{ _id: 4 }, { _id: 4, x: 1 }, {upsert:true}], {acknowledged:true, matchedCount:0, modifiedCount:0, upsertedId: 4}, [{_id:1, x: 11}, {_id:2, x: 22}, {_id:3, x: 33}, {_id:4, x: 1}]);
 
 
