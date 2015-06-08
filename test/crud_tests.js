00dc433fcb58e917def2b6920f5d841746fb19e1
Christian Kvalheim <christkv@gmail.com>
2015-06-08 13:39:30 +0200

    refactored test cases

--- test/crud_tests.js
+++ test/crud_tests.js
@@ -1,25 +1,52 @@
 print("crud_tests")
 
-//
-// deleteMany tests
-var data = [{_id: 1, x:11}, {_id: 2, x:22}, {_id: 3, x:33}];
+// Get the collection
 var col = db.crud_tests;
 
-// Drop the collection
-col.drop();
+// Setup
+var createTestExecutor = function(instance, method, data, setup, teardown) {
+  return function(args, result, state) {
+    // Execute setup
+    if(setup) setup(instance, method, data);
 
-// Setup of data
-col.insertMany(data);
+    // Execute the method with arguments
+    var r = instance[method].apply(instance, args);
+    // Assert equality
+    assert.docEq(result, r);
 
-// DeleteMany when many documents match
-var result = col.deleteMany({_id: { $gt: 1 }});
-assert(result.deletedCount == 2);
-var endState = col.find().toArray();
-assert(endState.length == 1);
-assert(endState[0]._id == 1);
-assert(endState[0].x == 11);
+    // Get all the results
+    var results = instance.find({}).toArray();
+    // Assert equality
+    assert.docEq(state, results);
+
+    // Execute teardown
+    if(teardown) teardown(instance, method, data);
+  }
+}
+
+var data = [{_id: 1, x:11}, {_id: 2, x:22}, {_id: 3, x:33}];
+var setup = function(col, method, data) {
+  col.remove({});
+  col.insertMany(data);  
+}
+
+// Setup executors
+var deleteManyExecutor = createTestExecutor(col, 'deleteMany', data, setup);
+var deleteOneExecutor = createTestExecutor(col, 'deleteOne', data, setup);
+
+//
+// deleteMany tests
+deleteManyExecutor([{ _id: { $gt: 1 } }], {deletedCount:2}, [{_id:1, x: 11}]);
+deleteManyExecutor([{ _id: 4 }], {deletedCount:0}, [{_id:1, x: 11}, {_id:2, x: 22}, {_id:3, x: 33}]);
+
+//
+// deleteOne tests
+deleteOneExecutor([{ _id: { $gt: 1 } }], {deletedCount:1}, [{_id:1, x: 11}, {_id:3, x: 33}]);
+deleteOneExecutor([{ _id: 2 }], {deletedCount:1}, [{_id:1, x: 11}, {_id:3, x: 33}]);
+deleteOneExecutor([{ _id: 4 }], {deletedCount:0}, [{_id:1, x: 11}, {_id:2, x: 22}, {_id:3, x: 33}]);
 
 
-// var db = db.getSiblingDB('test');
-// print(Object.keys(db))
-// db.t.test();
+
+
+
+
