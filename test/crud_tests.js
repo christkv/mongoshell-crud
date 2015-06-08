19ae0b637206e5c91d8b887ccc7a09f48e219ead
Christian Kvalheim <christkv@gmail.com>
2015-06-08 14:58:14 +0200

    Added findAndUpdate method and tests

--- test/crud_tests.js
+++ test/crud_tests.js
@@ -35,6 +35,7 @@ var deleteManyExecutor = createTestExecutor(col, 'deleteMany', data, setup);
 var deleteOneExecutor = createTestExecutor(col, 'deleteOne', data, setup);
 var findOneAndDeleteExecutor = createTestExecutor(col, 'findOneAndDelete', data, setup);
 var findOneAndReplaceExecutor = createTestExecutor(col, 'findOneAndReplace', data, setup);
+var findOneAndUpdateExecutor = createTestExecutor(col, 'findOneAndUpdate', data, setup);
 
 //
 // DeleteMany
@@ -79,7 +80,6 @@ findOneAndReplaceExecutor([{ _id: { $gt: 1 } }, { x: 32 }, { projection: { x: 1,
 findOneAndReplaceExecutor([{ _id: 2 }, { x: 32 }, { projection: { x: 1, _id: 0 }, sort: { x: 1 } }], {x:22}, [{_id:1, x: 11}, {_id:2, x: 32}, {_id:3, x: 33}]);
 // FindOneAndReplace when one document matches returning the document after modification
 findOneAndReplaceExecutor([{ _id: 2 }, { x: 32 }, { projection: { x: 1, _id: 0 }, sort: { x: 1 }, returnDocument:true }], {x:32}, [{_id:1, x: 11}, {_id:2, x: 32}, {_id:3, x: 33}]);
-
 // FindOneAndReplace when no documents match returning the document before modification
 findOneAndReplaceExecutor([{ _id: 4 }, { x: 44 }, { projection: { x: 1, _id: 0 }, sort: { x: 1 } }], null, [{_id:1, x: 11}, {_id:2, x: 22}, {_id:3, x: 33}]);
 // FindOneAndReplace when no documents match with upsert returning the document before modification
@@ -89,11 +89,29 @@ findOneAndReplaceExecutor([{ _id: 4 }, { x: 44 }, { projection: { x: 1, _id: 0 }
 // FindOneAndReplace when no documents match with upsert returning the document after modification
 findOneAndReplaceExecutor([{ _id: 4 }, { x: 44 }, { projection: { x: 1, _id: 0 }, sort: { x: 1 }, returnDocument:true, upsert:true }], {x:44}, [{_id:1, x: 11}, {_id:2, x: 22}, {_id:3, x: 33}, {_id:4, x: 44}]);
 
+//
+// FindOneAndUpdate
+//
 
-// // FindOneAndDelete when one document matches
-// findOneAndDeleteExecutor([{ _id: 2 }, { projection: { x: 1, _id: 0 }, sort: { x: 1 } }], {x:22}, [{_id:1, x: 11}, {_id:3, x: 33}]);
-// // FindOneAndDelete when no documents match
-// findOneAndDeleteExecutor([{ _id: 4 }, { projection: { x: 1, _id: 0 }, sort: { x: 1 } }], null, [{_id:1, x: 11}, {_id:2, x: 22}, {_id:3, x: 33}]);
+// FindOneAndUpdate when many documents match returning the document before modification
+findOneAndUpdateExecutor([{ _id: { $gt: 1 } }, { $inc: { x: 1 } }, { projection: { x: 1, _id: 0 }, sort: { x: 1 } }], {x:22}, [{_id:1, x: 11}, {_id:2, x: 23}, {_id:3, x: 33}]);
+// FindOneAndUpdate when many documents match returning the document after modification
+findOneAndUpdateExecutor([{ _id: { $gt: 1 } }, { $inc: { x: 1 } }, { projection: { x: 1, _id: 0 }, sort: { x: 1 }, returnDocument: true }], {x:23}, [{_id:1, x: 11}, {_id:2, x: 23}, {_id:3, x: 33}]);
+// FindOneAndUpdate when one document matches returning the document before modification
+findOneAndUpdateExecutor([{ _id: 2 }, { $inc: { x: 1 } }, { projection: { x: 1, _id: 0 }, sort: { x: 1 } }], {x:22}, [{_id:1, x: 11}, {_id:2, x: 23}, {_id:3, x: 33}]);
+// FindOneAndUpdate when one document matches returning the document after modification
+findOneAndUpdateExecutor([{ _id: 2 }, { $inc: { x: 1 } }, { projection: { x: 1, _id: 0 }, sort: { x: 1 }, returnDocument: true }], {x:23}, [{_id:1, x: 11}, {_id:2, x: 23}, {_id:3, x: 33}]);
+// FindOneAndUpdate when no documents match returning the document before modification
+findOneAndUpdateExecutor([{ _id: 4 }, { $inc: { x: 1 } }, { projection: { x: 1, _id: 0 }, sort: { x: 1 } }], null, [{_id:1, x: 11}, {_id:2, x: 22}, {_id:3, x: 33}]);
+// FindOneAndUpdate when no documents match with upsert returning the document before modification
+findOneAndUpdateExecutor([{ _id: 4 }, { $inc: { x: 1 } }, { projection: { x: 1, _id: 0 }, sort: { x: 1 }, upsert:true }], null, [{_id:1, x: 11}, {_id:2, x: 22}, {_id:3, x: 33}, {_id:4, x: 1}]);
+// FindOneAndUpdate when no documents match returning the document after modification
+findOneAndUpdateExecutor([{ _id: 4 }, { $inc: { x: 1 } }, { projection: { x: 1, _id: 0 }, sort: { x: 1 }, returnDocument:true }], null, [{_id:1, x: 11}, {_id:2, x: 22}, {_id:3, x: 33}]);
+// FindOneAndUpdate when no documents match with upsert returning the document after modification
+findOneAndUpdateExecutor([{ _id: 4 }, { $inc: { x: 1 } }, { projection: { x: 1, _id: 0 }, sort: { x: 1 }, returnDocument:true, upsert:true }], {x:1}, [{_id:1, x: 11}, {_id:2, x: 22}, {_id:3, x: 33}, {_id:4, x: 1}]);
+
+
+
 
 
 
